ad06c40d89a765937e3e3d89e75a0dc1bf63e179
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-10-20 17:05:00 +0200

    Adding two examples

---
+++ src/asc/examples/cello-suite.clj
@@ -0,0 +1,93 @@
+;; Score available at
+;; http://imslp.org/wiki/Cello_Suite_No.1_in_G_major,_BWV_1007_(Bach,_Johann_Sebastian)
+
+;; Simple and beautiful classical 4/4 song
+(ns asc.examples.cello-suite
+  (:use [asc.notation]))
+
+;; Avoid having to type (play foo 1/4) for each note
+(defmacro quarter-seq [& notes]
+  `(play-seq
+    ~@(map (fn [n] `(play ~n 1/4)) notes)))
+
+;;; First staff
+(defbar bar1 {}
+  (repeat-bars 2
+     (quarter-seq
+      :G2 :D3 :B3 :A3
+      :B3 :D3 :B3 :D3)))
+
+(defbar bar2 {}
+  (repeat-bars 2
+    (quarter-seq
+     :G2 :E3 :C4 :B3
+     :C4 :E3 :C4 :E3)))
+
+(defbar bar3 {}
+  (repeat-bars 2
+    (quarter-seq
+     :G2 :F#3 :C4 :B3
+     :C4 :F#3 :C4 :F#3)))
+
+;;; Second staff
+(defbar bar4 {}
+  (quarter-seq
+   :G2 :G3 :B3 :A3
+   :B3 :G3 :B3 :G3
+   :G2 :G3 :B3 :A3
+   :B3 :G3 :B3 :F#3))
+
+(defbar bar5 {}
+  (quarter-seq
+   :G2 :E3 :B3 :A3
+   :B3 :G3 :F#3 :G3
+   :E3 :G3 :F#3 :G3
+   :B3 :D3 :C#3 :B3))
+
+(defbar bar6 {}
+  (quarter-seq
+   :C#3 :G3 :A3 :G3
+   :A3 :G3 :A3 :G3
+   :C3 :G3 :A3 :G3
+   :A3 :G3 :A3 :G3))
+
+(defbar bar7 {}
+  (quarter-seq
+   :F#3 :A3 :D4 :C#4
+   :D4 :A3 :G3 :A3
+   :F#3 :A3 :G3 :A3
+   :D3 :F#3 :E3 :D3))
+
+;;; Third staff
+(defbar bar8 {}
+  (repeat-bars 2
+    (quarter-seq
+     :E2 :B2 :G3 :F#3
+     :G3 :B2 :G3 :B2)))
+
+(defbar bar9 {}
+  (quarter-seq
+   :E2 :C#3 :D3 :E3
+   :D3 :C3 :B2 :A2
+   :G3 :F#3 :E3 :D4
+   :C#4 :B3 :A3 :G3))
+
+(defbar bar10 {}
+  (quarter-seq
+   :F#3 :E3 :D3 :D4
+   :A3 :D4 :F#3 :A3
+   :D3 :E3 :F#3 :A3
+   :G3 :F#3 :E3 :D3))
+
+(defbar bar11 {}
+  (quarter-seq
+   :G#3 :D3 :F3 :E3
+   :F#3 :D3 :G#3 :D3
+   :B3 :D3 :F3 :E3
+   :F#3 :D3 :G#3 :D3))
+
+;;; Entire progression
+(defprog cello-suite
+  bar1 bar2 bar3
+  bar4 bar5 bar6 bar7
+  bar8 bar9 bar10 bar11)
---
+++ src/asc/examples/spring.clj
@@ -0,0 +1,104 @@
+;;; Score available at
+;;; http://imslp.org/wiki/Violin_Concerto_in_E_major,_RV_269_(Vivaldi,_Antonio)#Recordings
+
+(ns asc.examples.spring
+  (:use [asc.notation]
+        [overtone.inst.synth]
+        [overtone.inst.sampled-piano]))
+
+;;; Main violin
+(defbar bar0-MV {}
+  (play-seq
+   (play :rest 3.5)
+   (play :E4 1/2)))
+
+(defbar bar1-MV {}
+  (play-seq
+   (play :G#4 1/2)
+   (play :G#4 1/2)
+   (play :G#4 1/2)
+   (play :F#4 1/4)
+   (play :E4 1/4)
+   (play :B4 3/2)
+   (play :B4 1/4)
+   (play :A4 1/4)))
+
+(defbar bar3-MV {}
+  (play-seq
+   (play :G#4 1/2)
+   (play :A4 1/4)
+   (play :B4 1/4)
+   (play :A4 1/2)
+   (play :G#4 1/2)
+   (play :F#4 1/2)
+   (play :D#4 1/2)
+   (play :B3 1/2)
+   (play :E4 1/2)))
+
+(defbar bar6-MV {}
+  (play-seq
+   (play :G#4 1/2)
+   (play :A4 1/4)
+   (play :B4 1/4)
+   (play :A4 1/2)
+   (play :G#4 1/2)
+   (play :F#4 1)
+   (play :rest 1/4)
+   (play :E4 1/4)))
+
+(defprog main-violin
+  bar0-MV bar1-MV bar1-MV bar3-MV
+  bar1-MV bar1-MV bar6-MV)
+
+;;; First violin
+(defprog first-violin
+  bar0-MV bar1-MV bar1-MV bar3-MV
+  bar1-MV bar1-MV bar6-MV)
+
+;;; Second violin
+(defbar bar0-SV {}
+  (play-seq
+   (play :rest 3.5)
+   (play :B3 1/2)))
+
+(defbar bar1-SV {}
+  (play-seq
+   (play :E4 1/2)
+   (play :E4 1/2)
+   (play :E4 1/2)
+   (play :E4 1/2)
+   (play :G#4 3/2)
+   (play :G#4 1/4)
+   (play :F#4 1/4)))
+
+(defbar bar3-SV {}
+  (play-seq
+   (play :E4 1/2)
+   (play :F#4 1/4)
+   (play :G#4 1/4)
+   (play :F#4 1/2)
+   (play :E4 1/2)
+   (play :D#4 1)
+   (play :rest 1/2)
+   (play :B3 1/2)))
+
+(defbar bar6-SV {}
+  (play-seq
+   (play :E4 1/2)
+   (play :F#4 1/4)
+   (play :G#4 1/4)
+   (play :F#4 1/2)
+   (play :E4 1/2)
+   (play :D#4 1)
+   (play :rest 1/2)
+   (play :G#4 1/2)))
+
+(defprog second-violin
+  bar0-SV bar1-SV bar1-SV bar3-SV
+  bar1-SV bar1-SV bar6-SV)
+
+;;; Full song
+(defsong spring
+  [main-violin pad]
+  [first-violin pad]
+  [second-violin pad])
