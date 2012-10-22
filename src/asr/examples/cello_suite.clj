b16f21d2d2ce1d485df2ad702374364a44961252
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-10-22 20:32:37 +0200

    Updating examples

--- src/asr/examples/cello_suite.clj
+++ src/asr/examples/cello_suite.clj
@@ -6,82 +6,77 @@
   (:use [asr.notation]
         [overtone.inst.sampled-piano]))
 
-;; Avoid having to type (play foo 1/4) for each note
-(defmacro quarter-seq [& notes]
-  `(play-seq
-    ~@(map (fn [n] `(play ~n 1/4)) notes)))
-
 ;;; First staff
-(defbar bar1 {}
-  (repeat-bars 2
-     (quarter-seq
+(defbar bar1
+  (repeat-elements 2
+     (simple-seq 1/4
       :G2 :D3 :B3 :A3
       :B3 :D3 :B3 :D3)))
 
-(defbar bar2 {}
-  (repeat-bars 2
-    (quarter-seq
+(defbar bar2
+  (repeat-elements 2
+    (simple-seq 1/4
      :G2 :E3 :C4 :B3
      :C4 :E3 :C4 :E3)))
 
-(defbar bar3 {}
-  (repeat-bars 2
-    (quarter-seq
+(defbar bar3
+  (repeat-elements 2
+    (simple-seq 1/4
      :G2 :F#3 :C4 :B3
      :C4 :F#3 :C4 :F#3)))
 
 ;;; Second staff
-(defbar bar4 {}
-  (quarter-seq
+(defbar bar4
+  (simple-seq 1/4
    :G2 :G3 :B3 :A3
    :B3 :G3 :B3 :G3
    :G2 :G3 :B3 :A3
    :B3 :G3 :B3 :F#3))
 
-(defbar bar5 {}
-  (quarter-seq
+(defbar bar5
+  (simple-seq 1/4
    :G2 :E3 :B3 :A3
    :B3 :G3 :F#3 :G3
    :E3 :G3 :F#3 :G3
    :B3 :D3 :C#3 :B3))
 
-(defbar bar6 {}
-  (quarter-seq
+(defbar bar6
+  (simple-seq 1/4
    :C#3 :G3 :A3 :G3
    :A3 :G3 :A3 :G3
    :C3 :G3 :A3 :G3
    :A3 :G3 :A3 :G3))
 
-(defbar bar7 {}
-  (quarter-seq
+(defbar bar7
+  (simple-seq 1/4
    :F#3 :A3 :D4 :C#4
    :D4 :A3 :G3 :A3
    :F#3 :A3 :G3 :A3
    :D3 :F#3 :E3 :D3))
 
 ;;; Third staff
-(defbar bar8 {}
-  (repeat-bars 2
-    (quarter-seq
+(defbar bar8
+  (repeat-elements 2
+    (simple-seq 1/4
      :E2 :B2 :G3 :F#3
      :G3 :B2 :G3 :B2)))
 
-(defbar bar9 {}
-  (quarter-seq
+(defbar bar9
+  (simple-seq 1/4
    :E2 :C#3 :D3 :E3
    :D3 :C3 :B2 :A2
    :G3 :F#3 :E3 :D4
    :C#4 :B3 :A3 :G3))
 
-(defbar bar10 {}
-  (quarter-seq
+(defbar bar10
+  (simple-seq 1/4
    :F#3 :E3 :D3 :D4
    :A3 :D4 :F#3 :A3
    :D3 :E3 :F#3 :A3
    :G3 :F#3 :E3 :D3))
 
-(defbar bar11 {}
-  (quarter-seq
+(defbar bar11
+  (simple-seq 1/4
    :G#3 :D3 :F3 :E3
    :F#3 :D3 :G#3 :D3
    :B3 :D3 :F3 :E3
--- src/asr/examples/vivaldi.clj
+++ src/asr/examples/vivaldi.clj
@@ -1,29 +1,24 @@
 ;;; Score available at
 ;;; http://imslp.org/wiki/Violin_Concerto_in_E_major,_RV_269_(Vivaldi,_Antonio)#Recordings
 
-(ns asr.examples.spring
+(ns asr.examples.vivaldi
   (:use [asr.notation]
-        [overtone.inst.synth]
-        [overtone.inst.sampled-piano]))
+        [overtone.inst.synth]))
 
 ;;; Main violin
-(defbar bar0-MV {}
-  (play-seq
-   (play :rest 3.5)
-   (play :E4 1/2)))
+(defbar bar0-MV
+  (simple-seq
+   3.5 :rest
+   1/2 :E4))
 
-(defbar bar1-MV {}
-  (play-seq
-   (play :G#4 1/2)
-   (play :G#4 1/2)
-   (play :G#4 1/2)
-   (play :F#4 1/4)
-   (play :E4 1/4)
-   (play :B4 3/2)
-   (play :B4 1/4)
-   (play :A4 1/4)))
+(defbar bar1-MV
+  (simple-seq
+   1/2 :G#4 :G#4 :G#4
+   1/4 :F#4 :E4
+   3/2 :B4
+   1/4 :B4 :A4))
 
-(defbar bar3-MV {}
+(defbar bar3-MV
   (play-seq
    (play :G#4 1/2)
    (play :A4 1/4)
@@ -35,7 +30,7 @@
    (play :B3 1/2)
    (play :E4 1/2)))
 
-(defbar bar6-MV {}
+(defbar bar6-MV
   (play-seq
    (play :G#4 1/2)
    (play :A4 1/4)
@@ -56,12 +51,12 @@
   bar1-MV bar1-MV bar6-MV)
 
 ;;; Second violin
-(defbar bar0-SV {}
+(defbar bar0-SV
   (play-seq
    (play :rest 3.5)
    (play :B3 1/2)))
 
-(defbar bar1-SV {}
+(defbar bar1-SV
   (play-seq
    (play :E4 1/2)
    (play :E4 1/2)
@@ -71,7 +66,7 @@
    (play :G#4 1/4)
    (play :F#4 1/4)))
 
-(defbar bar3-SV {}
+(defbar bar3-SV
   (play-seq
    (play :E4 1/2)
    (play :F#4 1/4)
@@ -82,7 +77,7 @@
    (play :rest 1/2)
    (play :B3 1/2)))
 
-(defbar bar6-SV {}
+(defbar bar6-SV
   (play-seq
    (play :E4 1/2)
    (play :F#4 1/4)
@@ -98,7 +93,7 @@
   bar1-SV bar1-SV bar6-SV)
 
 ;;; Full song
-(defsong spring
+(defsong vivaldi
   [main-violin pad]
   [first-violin pad]
   [second-violin pad])
