35bc23cd0a41e70d775e79ffc18c0939bfa9c6cd
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-11-26 18:30:03 +0100

    take orientation into account

--- src/overscore/tools/audiveris.clj
+++ src/overscore/tools/audiveris.clj
@@ -8,23 +8,36 @@
            (javax.imageio ImageIO)
            (java.io File)))
 
-(defrecord run [x y length])
+(defrecord run [x y length orientation])
 
 (def black 0x000000)
 (def white 0xFFFFFF)
 
 (defn extract-run
   "Extract one run from its XML representation"
-  [run first-pos]
-  (->run first-pos
-         (parse-int (:start (:attrs run)))
-         (parse-int (:length (:attrs run)))))
+  [run orientation first-pos]
+  (->run (case orientation
+           :horizontal (parse-int (:start (:attrs run)))
+           :vertical first-pos)
+         (case orientation
+           :horizontal first-pos
+           :vertical (parse-int (:start (:attrs run))))
+         (parse-int (:length (:attrs run)))
+         orientation))
+
+(defn parse-orientation
+  "Return the orientation corresponding to its string representation"
+  [s]
+  (case s
+    "HORIZONTAL" :horizontal
+    "VERTICAL" :vertical))
 
 (defn extract-runs-from-section
   "Extract all the runs of an XML representation of a section"
   [section]
-  (let [first-pos (parse-int (:first-pos (:attrs section)))]
-    (map #(extract-run %1 (+ first-pos %2))
+  (let [first-pos (parse-int (:first-pos (:attrs section)))
+        orientation (parse-orientation (:orientation (:attrs section)))]
+    (map #(extract-run %1 orientation (+ first-pos %2))
          (:content section)
          (iterate inc 0))))
 
@@ -39,8 +52,13 @@
 (defn draw-run
   "Draw a run in an image"
   [run img]
-  (doseq [y (range (:y run) (+ (:y run) (:length run)))]
-    (.setRGB img (:x run) y black)))
+  (case (:orientation run)
+    :horizontal
+    (doseq [x (range (:x run) (+ (:x run) (:length run)))]
+      (.setRGB img x (:y run) black))
+    :vertical
+    (doseq [y (range (:y run) (+ (:y run) (:length run)))]
+      (.setRGB img (:x run) y black))))
 
 (defn draw-runs
   "Draw all the runs from an XML representation into an image"
@@ -53,7 +71,8 @@
   [x y run]
   (->run (- (:x run) x)
          (- (:y run) y)
-         (:length run)))
+         (:length run)
+         (:orientation run)))
 
 (defn fill
   "Fill an image with a certain color"
@@ -62,19 +81,26 @@
           y (range (.getHeight img))]
     (.setRGB img x y color)))
 
+(defn find-dimensions
+  "Find the dimensions of an image given its runs"
+  [runs]
+  (let [xs (map :x (sort #(compare (:x %1) (:x %2)) runs))
+        x (first xs)
+        width (+ 1 (- (last xs) x))
+        y (:y (first (sort #(compare (:y %1) (:y %2)) runs)))
+        last-run (last (sort #(compare (+ (:y %1) (:length %1))
+                                       (+ (:y %2) (:length %2)))
+                             runs))
+        height (+ 1 (- (+ (:y last-run) (:length last-run)) y))]
+    [x y width height]))
+
 (defn to-image
   "Convert an XML representation to an image"
   [in out]
   (let [xml (xml/parse in)
         original-runs (extract-all-runs xml)
-        xs (map :x (sort #(compare (:x %1) (:x %2)) original-runs))
-        x (first xs)
-        width (+ 1 (- (last xs) x))
-        y (:y (first (sort #(compare (:y %1) (:y %2)) original-runs)))
-        last-run (last (sort #(compare (+ (:y %1) (:length %1))
-                                     (+ (:y %2) (:length %2)))
-                           original-runs))
-        height (+ 1 (- (+ (:y last-run) (:length last-run)) y))
+        [x y width height] (find-dimensions original-runs)
+
         runs (map #(adjust-run x y %) original-runs)
         img (BufferedImage. width height BufferedImage/TYPE_BYTE_BINARY)]
     (fill img white)
