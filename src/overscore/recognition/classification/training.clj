4a65fb02a11531f7d59a663fc42910801d9d92c8
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-08 15:53:51 +0100

    Store training data as vector instead of images

--- src/overscore/recognition/classification/training.clj
+++ src/overscore/recognition/classification/training.clj
@@ -1,14 +1,35 @@
 ;;; Define and load the training set data
 (ns overscore.recognition.classification.training
+  (:use overscore.recognition.segmentation.segment)
   (:import java.awt.image.BufferedImage
            javax.imageio.ImageIO
            java.io.File))
 
-(defrecord training-data [class image])
+(defrecord training-data [class width height data])
 
 (def training-set (atom []))
 
-(defn load-training-set
+(defn to-vector
+  "Convert a BufferedImage to a one-dimension boolean vector, where
+  the true values correspond to activated pixels (thus, black
+  pixels). If a segment is given, only return the data corresponding
+  to that segment"
+  ([^BufferedImage img]
+     (to-vector img (->segment 0 (.getWidth img) 0 (.getHeight img))))
+  ([^BufferedImage img segment]
+     (loop [x (:start-x segment)
+            y (:start-y segment)
+            res (transient [])]
+       (if (< y (+ (:start-y segment) (segment-height segment)))
+         (if (< x (+ (:start-x segment) (segment-width segment)))
+           (recur (inc x) (inc y)
+                  (conj! res (if (== (.getRGB img x y) -1)
+                               false    ; white
+                               true)))  ; black
+           (recur 0 (inc y) res))
+         (persistent! res)))))
+
+(defn load-training-set-images
   "Load the training set data from a given directory. The structure of
   the data directory should be as follow:
 
@@ -17,8 +38,13 @@
 
     - The subdirectories contains png image representing the class of
       this subdirectory (the name of the files is not important
-      there)"
-  [in]
+      there)
+
+  Store the data in training-set, stored as training-data records with
+  the data element set as the result of calling the store function
+  with the BufferedImage corresponding to the current image."
+  [in & {:keys [store]
+         :or {store to-vector}}]
   (if (empty? @training-set)
     (let [dir (File. in)]
       (doseq [subdir (.listFiles dir)]
@@ -26,8 +52,11 @@
           (doseq [file (.listFiles subdir)]
             (when (.isFile file)
               (let [file (File. (str in "/" (.getName subdir) "/" (.getName file)))
+                    img (ImageIO/read file)
                     data (->training-data
                           (keyword (.getName subdir))
-                          (ImageIO/read file))]
+                          (.getWidth img)
+                          (.getHeight img)
+                          (store img))]
                 (swap! training-set #(cons data %))))))))
     (println "Training set already loaded")))
