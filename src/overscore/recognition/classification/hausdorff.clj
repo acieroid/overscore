4f65c07f128a637c04f5c57a32a2270245dc2307
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-08 15:54:47 +0100

    Convert Hausdorff distance to use vectors instead of images

--- src/overscore/recognition/classification/hausdorff.clj
+++ src/overscore/recognition/classification/hausdorff.clj
@@ -1,6 +1,7 @@
 ;;; Compute the Hausdorff distance between two images
 (ns overscore.recognition.classification.hausdorff
-  (:use overscore.recognition.segmentation.segment)
+  (:use overscore.recognition.segmentation.segment
+        overscore.recognition.classification.training)
   (:import java.awt.image.BufferedImage))
 
 (defrecord point [x y])
@@ -14,45 +15,44 @@
 
 (defn black-points
   "Return the list of black points in an image (described by its
-  width, height and a function that return the color of a given pixel"
-  [w h f]
+  width, height and pixel vector"
+  [w h v]
   (loop [x 0
          y 0
+         v v
          res (transient [])]
     (if (< y h)
       (if (< x w)
-        (recur (inc x) (inc y)
-               (if (not (== (f x y) -1))
+        (recur (inc x) (inc y) (rest v)
+               (if (first v)
                  (conj! res (->point x y))
                  res))
-        (recur 0 (inc y) res))
+        (recur 0 (inc y) v res))
       (persistent! res))))
 
 (defn directed-hausdorff-distance
   "Compute the directed Hausdorff distance between two images, given
   their size (width, height) and a function that returns a pixel value
   given the coordinates in the image"
-  [aw ah af bw bh bf]
+  [aw ah av bw bh bv]
   (reduce max
           (map #(reduce min
                         (map point-distance
                              (repeat %)
-                             (black-points bw bh bf)))
-               (black-points aw ah af))))
+                             (black-points bw bh bv)))
+               (black-points aw ah av))))
 
 (defn hausdorff-distance
   "Compute the undirected Hausdorff distance between two image. The
   template image is an element from the training set, and the other
   image is represented by a (L2) segment of an image"
   [^BufferedImage image segment template]
-  (let [tw (.getWidth template)
-        th (.getHeight template)
-        tf #(.getRGB template %1 %2)
+  (let [tw (:width template)
+        th (:height template)
+        tv (:data template)
         sw (segment-width segment)
         sh (segment-height segment)
-        sf #(.getRGB image
-                     (+ (:start-x segment) %1)
-                     (+ (:start-y segment) %2))]
+        sv (to-vector image segment)]
     (max
-     (directed-hausdorff-distance tw th tf sw sh sf)
-     (directed-hausdorff-distance sw sh sf tw th tf))))
+     (directed-hausdorff-distance tw th tv sw sh sv)
+     (directed-hausdorff-distance sw sh sv tw th tv))))
