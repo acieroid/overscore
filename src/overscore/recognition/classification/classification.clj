5030ad52ba417d673e3b40a2feb87de72d115c0e
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-15 13:35:25 +0100

    Fix classification step

--- src/overscore/recognition/classification/classification.clj
+++ src/overscore/recognition/classification/classification.clj
@@ -1,5 +1,6 @@
-(ns overscore.recognition.classification.knn
-  (:use overscore.recognition.segmentation.segment
+(ns overscore.recognition.classification.classification
+  (:use overscore.tools.files
+        overscore.recognition.segmentation.segment
         overscore.recognition.classification.training
         overscore.recognition.classification.knn)
   (:import javax.imageio.ImageIO
@@ -10,13 +11,16 @@
   image"
   [in-img in-segments out]
   (let [img (ImageIO/read (File. in-img))
-        segments-vectors (read-vectors in-segments)
-        segments (map #(apply ->segment) segments-vectors)
-        classes (map #(classify img %) segments)
+        segments-vectors (read-vector in-segments)
+        segments (map (fn [[sx sy ex ey]]
+                        (->segment sx sy ex ey))
+                      segments-vectors)
+        classes (map #(let [x 0]
+                        (classify img %)) segments)
         output (map (fn [seg class] [(:start-x seg)
                                      (:start-y seg)
                                      (:end-x seg)
                                      (:end-y seg)
                                      class])
                     segments classes)]
-    (write-vectors out output)))
+    (write-vector out output)))
