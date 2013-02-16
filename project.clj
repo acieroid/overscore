0e49959a7eb2ce10119a353f41cdfc2f8c7ae977
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-16 16:15:50 +0100

    Add encog to project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [overtone "0.7.1"]]
+                 [overtone "0.7.1"]
+                 ;[incanter "1.5.0-SNAPSHOT"]
+                 [org.encog/encog-core "3.1.0"]
+                 [enclog "0.5.8-SNAPSHOT"]]
+  ;:source-paths ["opencv-2.4.9.jar"]
+  ;:jvm-opts ["-Djava.library.path=/usr/local/lib"]
   :offline? true
   :main overscore.main)
