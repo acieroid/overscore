07cd04490c46ceaa4cb4344f80a525fc49a02afd
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-24 16:35:01 +0100

    Improve NN classification

--- src/overscore/recognition/classification/nn.clj
+++ src/overscore/recognition/classification/nn.clj
@@ -40,19 +40,43 @@
   "Train a neural network until the error is less than err"
   [err & {:keys [network training-set]}]
   (let [trainer (Backpropagation. network training-set)]
-    (println "starting training")
+    (println "starting training, until error is" err)
     (.iteration trainer)
     (while (> (.getError trainer) err)
-      (println "training, error is " (.getError trainer))
-      (.iteration trainer))))
+      (println "training, error is" (.getError trainer))
+      (.iteration trainer))
+    (println "done training, final error:" (.getError trainer))))
 
-(def net
-  (network
-   :input 400
-   :output 1
-   :hidden [400]))
+(def net (atom nil))
+(def labels (atom []))
 
-(def labels (atom {}))
+(defn class-to-vector
+  "Convert a certain class (eg :g_clef) to a vector that represents
+  it (eg. [0 0 1 0 …])"
+  [class]
+  (let [index (first (keep-indexed #(when (= %2 class) %1) @labels))]
+    (if index
+      (concat
+       (take index (repeat 0.0))
+       [1.0]
+       (take (- (count @labels) (inc index)) (repeat 0.0)))
+      (println "Invalid class: " class))))
+
+(defn vector-to-class
+  "Convert a vector returned by the neural network to a class"
+  [vector & {:keys [threshold]
+             :or {threshold 0.5}}]
+  (loop [vector vector
+         i 0
+         max-i -1
+         max threshold]
+    (if (empty? vector)
+      (if (== max-i -1)
+        :empty
+        (nth @labels max-i))
+      (if (> (first vector) max)
+        (recur (rest vector) (inc i) i (first vector))
+        (recur (rest vector) (inc i) max-i max)))))
 
 (defn train-network
   "Train the neural network with the training set data"
@@ -60,20 +84,16 @@
   (let [input (map :data @training-set)
         output (map :class @training-set)
         syms (keys (group-by (fn [x] x) output))
+        _ (swap! labels (fn [_] syms))
         dataset (dataset input
-                         (map (fn [x] [(double x)]) (range (count output))))
-        new-labels (reduce (fn [map [sym val]] (assoc map val sym))
-                           (hash-map)
-                           (map (fn [x y] [x y]) syms (range)))]
-    (swap! labels (fn [_] new-labels))
-    (train 0.05 :network net :training-set dataset)))
+                         (map class-to-vector output))]
+    (swap! net (fn [_] (network :input 400 :output (count syms) :hidden [400])))
+    (train 0.05 :network @net :training-set dataset)))
 
 (defn classify-nn
   "Classify a symbol using the trained neural network"
   [^BufferedImage img segment]
   (let [input (data (resize-to-vector img segment))
-        class-id (int (.getData (.compute net input) 0))
-        class (get @labels class-id)]
-    (if (nil? class)
-      :empty
-      class)))
+        vector (into [] (.getData (.compute @net input)))
+        class (vector-to-class vector)]
+    class))
